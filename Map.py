5aad2d2be1c6cb6691465efce029e071e4184f82
Steinar22 <sj7821054@gmail.com>
2024-08-19 10:45:42 +0530

    first commit

---
+++ Map.py
@@ -0,0 +1,10 @@
+
+class Map(object):
+  def __init__(self , forest , cabin_lake , mountain) :
+    self.forest = forest
+    self.cabin_lake = cabin_lake
+    self.mountain = mountain
+    
+  def choose_map(self):
+    print("You are on Road 66 choose the route")
+    
